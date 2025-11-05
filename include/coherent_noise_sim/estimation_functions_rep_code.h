7304b05321f657c1d279906177f3670548ccea32
Eva Takou <etakou@vt.edu>
2025-11-05 11:31:31 -0500

    fixed bug

--- include/coherent_noise_sim/estimation_functions_rep_code.h
+++ include/coherent_noise_sim/estimation_functions_rep_code.h
@@ -1,9 +1,8 @@
-// estimation_fuctions_rep_code.h
-
 #pragma once
 #include <vector>
 #include <tuple>
 #include "PrecisionOfTypes.h"
 
 
-std::tuple<std::vector<Real>,std::vector<Real>,std::vector<Real>> estimate_edges_rep_code(const std::vector<std::vector<uint8_t>>& batch, int d, int n_stabs, int rds);
+std::tuple<std::vector<Real>,std::vector<Real>,std::vector<Real>> estimate_edges_rep_code(const std::vector<std::vector<uint8_t>>& batch, int d, int n_stabs, int rds,
+                                                                                          uint8_t include_higher_order, uint8_t print_higher_order);
