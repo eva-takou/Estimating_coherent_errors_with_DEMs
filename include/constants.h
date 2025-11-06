4abb8bec18311ab700795b1e5b8e7551666fe725
Eva Takou <etakou@vt.edu>
2025-11-06 15:44:29 -0500

    added constants used in the codes

---
+++ include/constants.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "PrecisionOfTypes.h"
+
+constexpr Real PI        = Real(3.1415926535897932384626);
+constexpr Real SQRT2     = Real(1.4142135623730951);
+constexpr Real SQRT2_INV = Real(0.7071067811865475);
+
