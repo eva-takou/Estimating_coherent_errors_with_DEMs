1329c286d806f981504b5ebd7869550c5bc91b9e
Eva Takou <etakou@vt.edu>
2025-11-07 10:31:52 -0500

    fixed bug

--- include/estimation_functions_rep_code.h
+++ include/estimation_functions_rep_code.h
@@ -4,5 +4,6 @@
 #include "PrecisionOfTypes.h"
 
 
-std::tuple<std::vector<Real>,std::vector<Real>,std::vector<Real>> estimate_edges_rep_code(const std::vector<std::vector<uint8_t>>& batch, int d, int n_stabs, int rds,
-                                                                                          uint8_t include_higher_order, uint8_t print_higher_order);
+
+std::tuple<std::vector<Real>,std::vector<Real>,std::vector<Real>> estimate_edges_rep_code(const std::vector<std::vector<uint8_t>>& batch, int d, int n_stabs, int rds, 
+                                                                                          bool include_higher_order, bool print_higher_order);
