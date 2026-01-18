73483de18f0faa557f0be8afc5fdb6aae70a10e7
Eva Takou <etakou@vt.edu>
2026-01-17 22:52:07 -0500

    building stochastic ops

---
+++ include/Stochastic_Ops.h
@@ -0,0 +1,6 @@
+
+#pragma once
+#include <vector>
+#include <Eigen/Dense>
+
+inline void apply_stochastic_Z_on_qubits(VectorXc& psi,int nQ, const std::vector<int>& qubits, const std::vector<Real>& prob_Z);
