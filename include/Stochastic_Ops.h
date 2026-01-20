8a0707a1ae439906e3920611a7cc30fb891b2780
Eva Takou <etakou@vt.edu>
2026-01-20 13:30:57 -0500

    added function for X stochastic flips

--- include/Stochastic_Ops.h
+++ include/Stochastic_Ops.h
@@ -35,8 +35,42 @@ inline void apply_stochastic_Z_on_qubits(VectorXc& psi, const std::vector<int>& 
 }
 
 
+inline void apply_stochastic_X_on_qubits(VectorXc& psi, const std::vector<int>& qubits, const std::vector<Real>& prob_X, int nQ){
+    
+    std::vector<int> qubits_to_apply_X;
+
+    std::uniform_real_distribution<double> dist(0.0, 1.0);    
 
 
+    for (int q = 0; q < qubits.size(); ++q) {
+        double r = dist(rng);   
+        if (r < prob_X[q]) {
+            qubits_to_apply_X.push_back(qubits[q]);
+        }
+    }
+
+
+    Eigen::Index flip_mask = 0;
+
+    for (int i = 0; i < qubits_to_apply_X.size(); ++i) {
+        
+        int q = qubits_to_apply_X[i];
+        flip_mask |= (1ULL << (nQ - 1 - q)); //MSB ordering
+        
+    }
+
+    if (flip_mask == 0) return; 
+
+    
+    for (Eigen::Index j = 0; j < dim; ++j) {
+        Eigen::Index j_flip = j ^ flip_mask;
+        if (j < j_flip) {
+            std::swap(psi[j], psi[j_flip]);
+        }
+    }    
+
+
+}
 
 
 
