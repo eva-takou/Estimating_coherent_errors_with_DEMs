d7d958fa7a5b4c38a780f2b6a1022d21768f3b8a
Eva Takou <etakou@vt.edu>
2026-01-21 12:45:24 -0500

    fixed bug

--- src/Monte_Carlo_Sim_surf_code.cpp
+++ src/Monte_Carlo_Sim_surf_code.cpp
@@ -56,16 +56,16 @@ using namespace Eigen;
 
 
 
-struct VectorHash {
-    size_t operator()(const std::vector<uint8_t>& v) const {
-        std::hash<uint8_t> hasher;
-        size_t seed = 0;
-        for (uint8_t i : v) {
-            seed ^= hasher(i) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-        }
-        return seed;
-    }
-};
+// struct VectorHash {
+//     size_t operator()(const std::vector<uint8_t>& v) const {
+//         std::hash<uint8_t> hasher;
+//         size_t seed = 0;
+//         for (uint8_t i : v) {
+//             seed ^= hasher(i) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+//         }
+//         return seed;
+//     }
+// };
 
 struct DataOutcome {
     std::vector<uint8_t> bitstring;  // data qubit bitstring
